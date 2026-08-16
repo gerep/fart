96ef6647807002ef6dea5168da766b25dbbe2c06
gerep <181501+gerep@users.noreply.github.com>
2026-08-16 18:21:33 -0300

    turret shooting frequency

--- scenes/turrets/turret.gd
+++ scenes/turrets/turret.gd
@@ -3,17 +3,22 @@ extends Node2D
 const DEFAULT_BULLET = preload("res://scenes/bullets/bullet.tscn")
 
 @export var rotation_speed: float = 8.0
+@export var shot_interval: float = 0.15
 
 @onready var range_area: Area2D = $RangeArea
 
 var _has_fired := false
+var _has_target := false
+var _target: Area2D
 var _target_global_rotation: float
 var _target_position: Vector2
 var _is_aiming := false
+var _shot_timer := 0.0
 
 
 func _ready() -> void:
 	range_area.area_entered.connect(_area_entered)
+	range_area.area_exited.connect(_area_exited)
 	_target_global_rotation = global_rotation
 
 
@@ -26,7 +31,12 @@ func _process(delta: float) -> void:
 
 	if _is_aiming and absf(angle_difference(global_rotation, _target_global_rotation)) < 0.05:
 		_is_aiming = false
-		call_deferred("_spawn_bullet", _target_position)
+		_shot_timer = 0.0
+
+	if _has_target and not _is_aiming:
+		_shot_timer -= delta
+		if _shot_timer <= 0.0:
+			_fire_shot()
 
 
 func _area_entered(area: Area2D) -> void:
@@ -34,10 +44,28 @@ func _area_entered(area: Area2D) -> void:
 		return
 
 	_has_fired = true
+	_has_target = true
+	_target = area
 	_target_position = area.global_position
 	var direction := global_position.direction_to(_target_position)
 	_target_global_rotation = direction.angle() + PI / 2.0
 	_is_aiming = true
+
+
+func _area_exited(area: Area2D) -> void:
+	if area != _target:
+		return
+
+	_target = null
+	_has_fired = false
+	_has_target = false
+	_is_aiming = false
+	_shot_timer = 0.0
+
+
+func _fire_shot() -> void:
+	call_deferred("_spawn_bullet", _target_position)
+	_shot_timer = maxf(shot_interval, 0.0)
 
 
 func _spawn_bullet(target_position: Vector2) -> void:
